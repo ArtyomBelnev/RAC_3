ff96d94dc64d3e80bca0309ce28d977a2c17eff1
ArtyomBelnev <belnevtema@gmail.com>
2021-04-27 22:53:20 +0300

    popup ebechiu

--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -5,21 +5,23 @@ export function p_d() {
   elements.popup.style.visibility = 'visible'
   elements.popup.transition = 'all 0.8s ease 0s'
   elements.popup__content.classList.add('popup__content_disabled')
-  elements.popupImages.style.background = 'url(../src/images/tab.png) 50% 50% no-repeat'
-  elements.popupImages.style.height = '130px'
+  elements.popupTextNames.classList.add('disabled')
+
+  elements.infoImg.forEach((el) => {
+    if (el.classList.contains('info__img4')) {
+      el.classList.add('active')
+    } else el.classList.remove('active')
+  })
 }
 
 export function p_dd() {
   elements.popup.style.opacity = '1'
   elements.popup.style.visibility = 'visible'
   elements.popup.style.transition = 'all 0.8s ease 0s'
-  elements.popupTextNames.style.display = 'block'
-  elements.popupImages.style.height = '0'
-  elements.popupTextNames.style.display = null
-  elements.infoNO.style.color = null
-  elements.infoGP.style.color = null
-  elements.infoHP.style.color = null
   elements.popup__content.classList.add('popup__content_disabled')
+  elements.popupTextNames.classList.remove('disabled')
+
+  elements.infoImg.forEach((el) => el.classList.remove('active'))
 }
 
 export function pc_d(e) {
@@ -27,38 +29,23 @@ export function pc_d(e) {
     elements.popup.style.opacity = '0'
     elements.popup.style.visibility = 'hidden'
     elements.popup__content.classList.remove('popup__content_disabled')
-    elements.popupTextNames.style.display = 'none'
-    elements.popupImages.style.background = null
-    elements.popup.style.transition = null
   }
   if (e.target.className == 'popup__close') {
     elements.popup.style.opacity = '0'
     elements.popup.style.visibility = 'hidden'
     elements.popup__content.classList.remove('popup__content_disabled')
-    elements.popupTextNames.style.display = 'none'
-    elements.popupImages.style.background = null
-    elements.popup.style.transition = null
   }
 
-  if (e.target.id == 'info_HP') {
-    elements.popupImages.style.background = 'url(../src/images/1.png) 50% 50% no-repeat'
-    elements.popupImages.style.height = '500px'
-    elements.infoHP.style.color = 'rgb(184, 17, 17)'
-    elements.infoGP.style.color = null
-    elements.infoNO.style.color = null
-  }
-  if (e.target.id == 'info_GP') {
-    elements.popupImages.style.background = 'url(../src/images/2.png) 50% 50%no-repeat'
-    elements.popupImages.style.height = '500px'
-    elements.infoGP.style.color = 'rgb(184, 17, 17)'
-    elements.infoHP.style.color = null
-    elements.infoNO.style.color = null
-  }
-  if (e.target.id == 'info_NO') {
-    elements.popupImages.style.background = 'url(../src/images/3.png) 50% 50% no-repeat'
-    elements.popupImages.style.height = '500px'
-    elements.infoNO.style.color = 'rgb(184, 17, 17)'
-    elements.infoGP.style.color = null
-    elements.infoHP.style.color = null
+  if (e.target.className == 'info__link') {
+    let target = e.target.dataset.targ
+    elements.infoLink.forEach((el) => el.classList.remove('active'))
+    elements.infoImg.forEach((el) => el.classList.remove('active'))
+    e.target.classList.add('active')
+
+    elements.infoImg.forEach((el) => {
+      if (el.classList.contains(target)) {
+        el.classList.add('active')
+      }
+    })
   }
 }
