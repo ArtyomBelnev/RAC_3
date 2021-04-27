6aca168a62575faad39d9f0334116d77c0fc3060
ArtyomBelnev <https://github.com/ArtyomBelnev/JOBS>
2021-04-27 10:09:46 +0300

    preloder

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import '../styles/app.scss'
 import { elements } from './elements/Elements'
 import { getTumblers } from './components/Tumblers'
 import { getPowers } from './components/PowerUP'
-import { p_d, pc_d } from './components/Popup'
+import { p_d, p_dd, pc_d } from './components/Popup'
 import { errorActive } from './components/Journal'
 
 elements.tumBTN.addEventListener('click', getTumblers.bind(getTumblers))
@@ -11,6 +11,7 @@ elements.powers.addEventListener('click', getPowers.bind(getPowers))
 elements.popup__close.addEventListener('click', pc_d.bind(pc_d))
 elements.popup.addEventListener('click', pc_d.bind(pc_d))
 elements.info.addEventListener('click', p_d)
+elements.information.addEventListener('click', p_dd)
 elements.probTest.addEventListener('click', goTest)
 elements.kontrols.addEventListener('click', goKontorls)
 
--- src/js/components/Popup.js
+++ src/js/components/Popup.js
@@ -3,7 +3,17 @@ import { elements } from '../elements/Elements'
 export function p_d() {
   elements.popup.style.opacity = '1'
   elements.popup.style.visibility = 'visible'
+  elements.popup.transition = 'all 0.8s ease 0s'
   elements.popup__content.classList.add('popup__content_disabled')
+  elements.popupImages.style.background = 'url(../src/images/tab.png) no-repeat'
+}
+
+export function p_dd() {
+  elements.popup.style.opacity = '1'
+  elements.popup.style.visibility = 'visible'
+  elements.popup.transition = 'all 0.8s ease 0s'
+  elements.popup__content.classList.add('popup__content_disabled')
+  elements.popupImages.style.background = 'url(../src/images/fon.jpg) no-repeat'
 }
 
 export function pc_d(e) {
@@ -11,10 +21,14 @@ export function pc_d(e) {
     elements.popup.style.opacity = '0'
     elements.popup.style.visibility = 'hidden'
     elements.popup__content.classList.remove('popup__content_disabled')
+    elements.popupImages.style.background = null
+    elements.popup.transition = null
   }
   if (e.target.className == 'popup__close') {
     elements.popup.style.opacity = '0'
     elements.popup.style.visibility = 'hidden'
     elements.popup__content.classList.remove('popup__content_disabled')
+    elements.popupImages.style.background = null
+    elements.popup.transition = null
   }
 }
